fa5c26e68e4ae386a124b7fc3aad5bbb9617b2a2
ambtionGCZ <5231622+ambition_gcz@user.noreply.gitee.com>
2026-05-04 01:00:43 +0800

    fix: align formx dark theme components

--- docs/.vitepress/theme/index.ts
+++ docs/.vitepress/theme/index.ts
@@ -2,6 +2,7 @@ import type { Theme } from 'vitepress'
 import DefaultTheme from 'vitepress/theme'
 import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
+import 'element-plus/theme-chalk/dark/css-vars.css'
 import '../../../packages/vue-ep/src/style.css'
 import './styles.css'
 
